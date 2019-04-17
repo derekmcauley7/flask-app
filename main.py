cd0c8a3c9946ec79aec71b75547f5ab816abe65b
Derek McAuley <derekmcauley7@gmail.com>
2019-04-17 23:06:23 +0100

    Cleaning code

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 @app.route("/status")
 def index():
-    cpus, host_name, ip, mem_gib = get_variables()
+    cpus, host_name, ip, mem_gib = get_json_variables()
     data = ({"hostname": host_name,
              "ip_address": ip,
              "cpus": cpus,
              "memory": mem_gib,
              })
 
-    response = create_json_response(data)
+    response = create_response(data)
     return response
 
 
-def create_json_response(data):
+def create_response(data):
     response = app.response_class(
         response=json.dumps(data),
         status=200,
@@ -26,7 +26,7 @@ def create_json_response(data):
     return response
 
 
-def get_variables():
+def get_json_variables():
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
     mem_gib = mem_bytes / (1024. ** 3)
     host_name = request.base_url
