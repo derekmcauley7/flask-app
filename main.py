18a244194c4d18a9601dd0d08deea9a8b3f1d91f
Derek McAuley <derekmcauley7@gmail.com>
2019-04-19 19:49:22 +0100

    Using

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, json, request
+import subprocess
+from subprocess import PIPE
+from flask import Flask, json
 import os
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/status")
 def index():
@@ -15,7 +17,6 @@ def index():
     response = create_response(data)
     return response
 
-
 def create_response(data):
     response = app.response_class(
         response=json.dumps(data),
@@ -26,12 +27,36 @@ def create_response(data):
 
 
 def get_json_variables():
+    host_name = get_host_name()
+    ip = get_ip()
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
     mem_gib = mem_bytes / (1024. ** 3)
-    host_name = request.base_url
-    ip = request.host.split(':')[0]
-    cpus = os.cpu_count()
+    cpus = get_cpu()
     return cpus, host_name, ip, mem_gib
+
+
+def get_ip():
+    ip = subprocess.run(["hostname", "-I"], stdout=PIPE, stderr=PIPE)
+    stdout = ip.stdout.decode()
+    stdout.rstrip()
+    ip = stdout.rstrip()
+    return ip
+
+
+def get_host_name():
+    host_name = subprocess.run(["hostname"], stdout=PIPE, stderr=PIPE)
+    stdout = host_name.stdout.decode()
+    stdout.rstrip()
+    host_name = stdout.rstrip()
+    return host_name
+
+
+def get_cpu():
+    cpu = subprocess.run(["egrep", "-c", "^processor", "/proc/cpuinfo"], stdout=PIPE, stderr=PIPE)
+    stdout = cpu.stdout.decode()
+    stdout.rstrip()
+    cpu = stdout.rstrip()
+    return cpu
 
 
 if __name__ == '__main__':
